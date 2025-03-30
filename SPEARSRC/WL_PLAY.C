8b5eafe3f53fef37d51f146b729c98c21d03dbf5
ferahan <ferrangv@gmail.com>
2025-03-30 12:16:42 +0200

    Added strafe Left/Right
    Using default A and D keys, so left and right needs to be changed to other keys inside game settings for this to work as intended.

--- SPEARSRC/WL_PLAY.C
+++ SPEARSRC/WL_PLAY.C
@@ -74,7 +74,7 @@ memptr		demobuffer;
 //
 // curent user input
 //
-int			controlx,controly;		// range from -100 to 100 per tic
+int			controlx,controly,strafe;		// range from -100 to 100 per tic
 boolean		buttonstate[NUMBUTTONS];
 
 
@@ -207,7 +207,7 @@ int songs[]=
  XFUNKIE_MUS,
  XDEATH_MUS,
  XGETYOU_MUS,		// DON'T KNOW
- ULTIMATE_MUS,	// Trans Gr”sse
+ ULTIMATE_MUS,	// Trans Grï¿½sse
 
  DUNGEON_MUS,
  GOINGAFT_MUS,
@@ -350,6 +350,10 @@ void PollKeyboardMove (void)
 			controlx -= RUNMOVE*tics;
 		if (Keyboard[dirscan[di_east]])
 			controlx += RUNMOVE*tics;
+		if (Keyboard[sc_A])
+			strafe -= RUNMOVE*tics;
+		if (Keyboard[sc_D])
+			strafe += RUNMOVE*tics;
 	}
 	else
 	{
@@ -361,6 +365,10 @@ void PollKeyboardMove (void)
 			controlx -= BASEMOVE*tics;
 		if (Keyboard[dirscan[di_east]])
 			controlx += BASEMOVE*tics;
+		if (Keyboard[sc_A])
+			strafe -= BASEMOVE*tics;
+		if (Keyboard[sc_D])
+			strafe += BASEMOVE*tics;
 	}
 }
 
@@ -484,6 +492,7 @@ void PollControls (void)
 
 	controlx = 0;
 	controly = 0;
+	strafe = 0;
 	memcpy (buttonheld,buttonstate,sizeof(buttonstate));
 	memset (buttonstate,0,sizeof(buttonstate));
 
@@ -549,6 +558,11 @@ void PollControls (void)
 	else if (controly < min)
 		controly = min;
 
+	if (strafe > max)
+		strafe = max;
+	else if (strafe < min)
+		strafe = min;
+
 	if (demorecord)
 	{
 	//
@@ -556,6 +570,7 @@ void PollControls (void)
 	//
 		controlx /= (int)tics;
 		controly /= (int)tics;
+		strafe /= (int)tics;
 
 		buttonbits = 0;
 
@@ -569,12 +584,14 @@ void PollControls (void)
 		*demoptr++ = buttonbits;
 		*demoptr++ = controlx;
 		*demoptr++ = controly;
+		*demoptr++ = strafe;
 
 		if (demoptr >= lastdemoptr)
 			Quit ("Demo buffer overflowed!");
 
 		controlx *= (int)tics;
 		controly *= (int)tics;
+		strafe *= (int)tics;
 	}
 }
 
